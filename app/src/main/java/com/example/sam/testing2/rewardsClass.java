10f8369932ea5eee64380b8a698c24e9eaf62ce3
csammcgrath <mcg13004@byui.edu>
2016-11-30 21:23:23 -0700

    fixed up logo in navigation drawer and splashscreen

---
+++ app/src/main/java/com/example/sam/testing2/rewardsClass.java
@@ -0,0 +1,28 @@
+package com.example.sam.testing2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sam on 11/30/2016.
+ */
+
+public class rewardsClass extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        return inflater.inflate(R.layout.activity_rewards, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
