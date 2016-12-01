3ac1c6ae0cf821a706134bae35b7ae9323ec72e3
csammcgrath <mcg13004@byui.edu>
2016-11-30 20:28:51 -0700

    updated menu and locations

--- app/src/main/java/com/example/sam/testing2/homeScreen.java
+++ app/src/main/java/com/example/sam/testing2/homeScreen.java
@@ -1,11 +1,19 @@
 package com.example.sam.testing2;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.List;
 
 /**
  * Created by Sam on 11/21/2016.
@@ -25,5 +33,18 @@ public class homeScreen extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextView textBox = (TextView)getView().findViewById(R.id.tokenTextBox);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("KrazySub");
+
+        String token = getTokenFromPrefs();
+
+        textBox.setText(token);
+    }
+
+    private String getTokenFromPrefs()
+    {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        return preferences.getString("notification", null);
     }
 }
