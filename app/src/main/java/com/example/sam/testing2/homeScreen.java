8c3b07817f9cb2ecb76f616e7f8d98b2e75e85df
amartherus <andrew@martherus.com>
2016-12-02 15:42:23 -0700

    Added a welcome to the homescreen with the user's email address.

--- app/src/main/java/com/example/sam/testing2/homeScreen.java
+++ app/src/main/java/com/example/sam/testing2/homeScreen.java
@@ -10,6 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -20,6 +25,14 @@ import java.util.List;
  */
 
 public class homeScreen extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
+    private UserInfo userInfo;
+    private DatabaseReference databaseReference;
+    private Firebase firebase;
+
+    private TextView email;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,6 +45,27 @@ public class homeScreen extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //sets context to the current fragment
+        Firebase.setAndroidContext(getActivity());
+        //create new instance of userInfo class
+        userInfo = new UserInfo();
+
+        //this points to the database's URL
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser(); //current user's ID
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //the UID of the current user
+        DatabaseReference ref = database.getReference(user.getUid());
+
+        //connects the email variable to the respective textview (13)
+        email = (TextView) getView().findViewById(R.id.textView13);
+        //displays a welcome with the current user's ID
+        email.setText("Welcome " + user.getEmail() + "!");
+
 
 
     }
